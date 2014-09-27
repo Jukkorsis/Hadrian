45eb9d5c206d25153e7007d9ef3263f5f0e27ae7
Richard Thurston <richard@northernwall.com>
2014-09-27 13:44:29 -0700

    added standard header to all java files

--- src/main/java/com/northernwall/hadrian/handler/AvailabilityHandler.java
+++ src/main/java/com/northernwall/hadrian/handler/AvailabilityHandler.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2014 Richard Thurston.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.northernwall.hadrian.handler;
 
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +25,10 @@ import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ *
+ * @author Richard Thurston
+ */
 public class AvailabilityHandler extends AbstractHandler {
 
     private final static Logger logger = LoggerFactory.getLogger(AvailabilityHandler.class);
