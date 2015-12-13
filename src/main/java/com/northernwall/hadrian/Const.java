efd39072e33b549a73fdf2c952e94a05690b8ccd
Richard Thurston <richard@northernwall.com>
2015-12-13 14:56:45 -0800

    Minor reordering

--- src/main/java/com/northernwall/hadrian/Const.java
+++ src/main/java/com/northernwall/hadrian/Const.java
@@ -24,9 +24,6 @@ public class Const {
     public static final String TYPE_VIP = "vip";
     public static final String TYPE_HOST_VIP = "hostvip";
     
-    public static final String HOST_DETAILS_URL = "host.detailsUrl";
-    public static final String HOST_DETAILS_ATTRIBUTES = "host.detailsAttrs";
-
     public static final String ATTR_SESSION = "session";
     public static final String ATTR_USER = "user";
 
@@ -50,6 +47,9 @@ public class Const {
     public static final String JETTY_ACCEPT_QUEUE_SIZE = "jetty.idleTimeout";
     public static final String JETTY_ACCEPT_QUEUE_SIZE_DEFAULT = "100";
     
+    public static final String HOST_DETAILS_URL = "host.detailsUrl";
+    public static final String HOST_DETAILS_ATTRIBUTES = "host.detailsAttrs";
+
     public static final String WEB_HOOK_SENDER_FACTORY_CLASS_NAME = "webHookSender.factoryClassName";
     public static final String WEB_HOOK_SENDER_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.webhook.simple.SimpleWebHookSenderFactory";
 
@@ -61,36 +61,37 @@ public class Const {
     public static final String SIMPLE_WEB_HOOK_DELAY = "simpleWebhook.delay";
     public static final String SIMPLE_WEB_HOOK_DELAY_DEFAULT = "15";
 
-    public static final String MAVEN_URL = "maven.url";
-    public static final String MAVEN_URL_DEFAULT = "http://127.0.0.1/mvnrepo/internal/";
-    public static final String MAVEN_USERNAME = "maven.username";
-    public static final String MAVEN_USERNAME_DEFAULT = "-";
-    public static final String MAVEN_PASSWORD = "maven.password";
-    public static final String MAVEN_PASSWORD_DEFAULT = "-";
+    public static final String MAVEN_HELPER_FACTORY_CLASS_NAME = "maven.factoryClassName";
+    //public static final String MAVEN_HELPER_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.maven.ssh.SshMavenHelperFactory";
+    public static final String MAVEN_HELPER_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.maven.http.HttpMavenHelperFactory";
+
     public static final String MAVEN_MAX_VERSIONS = "maven.maxVersions";
     public static final String MAVEN_MAX_VERSIONS_DEFAULT = "15";
 
-    public static final String MAVEN_HELPER_FACTORY_CLASS_NAME = "maven.factoryClassName";
-    //public static final String MAVEN_HELPER_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.maven.SshMavenHelperFactory";
-    public static final String MAVEN_HELPER_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.maven.HttpMavenHelperFactory";
+    public static final String MAVEN_URL = "maven.http.url";
+    public static final String MAVEN_URL_DEFAULT = "http://127.0.0.1/mvnrepo/internal/";
+    public static final String MAVEN_USERNAME = "maven.http.username";
+    public static final String MAVEN_USERNAME_DEFAULT = "-";
+    public static final String MAVEN_PASSWORD = "maven.http.password";
+    public static final String MAVEN_PASSWORD_DEFAULT = "-";
 
     public static final String DATA_ACCESS_FACTORY_CLASS_NAME = "dataAccess.factoryClassName";
     public static final String DATA_ACCESS_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.db.InMemoryDataAccessFactory";
     //public static final String DATA_ACCESS_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.db.CassandraDataAccessFactory";
 
+    public static final String CASS_NODE = "dataAccess.cassandra.node";
+    public static final String CASS_NODE_DEFAULT = "127.0.0.1";
+    public static final String CASS_KEY_SPACE = "dataAccess.cassandra.keyspace";
+    public static final String CASS_KEY_SPACE_DEFAULT = "devops";
+    public static final String CASS_REPLICATION_FACTOR = "dataAccess.cassandra.replicationFactor";
+    public static final String CASS_REPLICATION_FACTOR_DEFAULT = "1";
+    
+    public static final String IN_MEMORY_DATA_FILE_NAME = "dataAccess.inMemory.dataFileName";
+    public static final String IN_MEMORY_DATA_FILE_NAME_DEFAULT = "data.json";
+    
     public static final String ACCESS_FACTORY_CLASS_NAME = "access.factoryClassName";
     public static final String ACCESS_FACTORY_CLASS_NAME_DEFAULT = "com.northernwall.hadrian.access.SimpleAccessFactory";
 
-    public static final String CASS_NODE = "cassandra.node";
-    public static final String CASS_NODE_DEFAULT = "127.0.0.1";
-    public static final String CASS_KEY_SPACE = "cassandra.keyspace";
-    public static final String CASS_KEY_SPACE_DEFAULT = "devops";
-    public static final String CASS_REPLICATION_FACTOR = "cassandra.replicationFactor";
-    public static final String CASS_REPLICATION_FACTOR_DEFAULT = "1";
-    
-    public static final String IN_MEMORY_DATA_FILE_NAME = "inMemory.dataFileName";
-    public static final String IN_MEMORY_DATA_FILE_NAME_DEFAULT = "data.json";
-    
     public static final String CONFIG_DATA_CENTERS = "config.dataCenters";
     public static final String CONFIG_DATA_CENTERS_DEFAULT = "dc";
     public static final String CONFIG_NETWORKS = "config.networks";
