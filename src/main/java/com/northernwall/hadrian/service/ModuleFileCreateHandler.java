bead8504e66ada3c3c8188892a936d0135a423b2
Richard Thurston <richard@northernwall.com>
2016-04-30 16:22:23 -0700

    Fixed bug in module file added auditing to module file

--- src/main/java/com/northernwall/hadrian/service/ModuleFileCreateHandler.java
+++ src/main/java/com/northernwall/hadrian/service/ModuleFileCreateHandler.java
@@ -17,16 +17,24 @@ package com.northernwall.hadrian.service;
 
 import com.northernwall.hadrian.access.AccessHelper;
 import com.northernwall.hadrian.db.DataAccess;
+import com.northernwall.hadrian.domain.Audit;
 import com.northernwall.hadrian.domain.Module;
 import com.northernwall.hadrian.domain.ModuleFile;
+import com.northernwall.hadrian.domain.Operation;
 import com.northernwall.hadrian.domain.Service;
+import com.northernwall.hadrian.domain.Type;
+import com.northernwall.hadrian.domain.User;
 import com.northernwall.hadrian.service.dao.PostModuleFileData;
 import com.northernwall.hadrian.utilityHandlers.routingHandler.Http400BadRequestException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jetty.server.Request;
+
+import static com.northernwall.hadrian.service.BasicHandler.getGson;
 
 /**
  *
@@ -46,35 +54,59 @@ public class ModuleFileCreateHandler extends BasicHandler {
         PostModuleFileData data = fromJson(request, PostModuleFileData.class);
         Service service = getService(data.serviceId, null, null);
         Module module = getModule(data.moduleId, null, service);
-        accessHelper.checkIfUserCanModify(request, service.getTeamId(), "manage file for module");
+        User user = accessHelper.checkIfUserCanModify(request, service.getTeamId(), "manage file for module");
         if (data.network == null || data.network.isEmpty()) {
             throw new Http400BadRequestException("attribute network is missing");
         }
 
         if (data.name == null || data.name.trim().isEmpty() || data.contents == null || data.contents.trim().isEmpty()) {
             ModuleFile moduleFile = getDataAccess().getModuleFile(service.getServiceId(), module.getModuleId(), data.network);
-            if (moduleFile == null) {
+            if (moduleFile != null) {
                 getDataAccess().deleteModuleFile(service.getServiceId(), module.getModuleId(), data.network, moduleFile.getName());
+                createAudit(service.getServiceId(), module.getModuleName(), user.getUsername(), "Deleted file " + moduleFile.getName() + " in " + data.network);
             }
         } else {
+            data.name = data.name.trim();
+            if (data.name.contains(" ")) {
+                throw new Http400BadRequestException("attribute name is illegal");
+            }
             ModuleFile moduleFile = getDataAccess().getModuleFile(service.getServiceId(), module.getModuleId(), data.network);
             if (moduleFile == null) {
                 moduleFile = new ModuleFile(service.getServiceId(), module.getModuleId(), data.network, data.name, data.contents);
                 getDataAccess().saveModuleFile(moduleFile);
+                createAudit(service.getServiceId(), module.getModuleName(), user.getUsername(), "Created file " + data.name + " in " + data.network);
             } else if (data.name.equalsIgnoreCase(moduleFile.getName())) {
                 if (!data.contents.equals(moduleFile.getContents())) {
                     moduleFile.setContents(data.contents);
                     getDataAccess().updateModuleFile(moduleFile);
-                }
+                    createAudit(service.getServiceId(), module.getModuleName(), user.getUsername(), "Updated file " + data.name + " in " + data.network);
+                  }
             } else {
                 getDataAccess().deleteModuleFile(service.getServiceId(), module.getModuleId(), data.network, moduleFile.getName());
+                createAudit(service.getServiceId(), module.getModuleName(), user.getUsername(), "Deleted file " + moduleFile.getName() + " in " + data.network);
                 moduleFile = new ModuleFile(service.getServiceId(), module.getModuleId(), data.network, data.name, data.contents);
                 getDataAccess().saveModuleFile(moduleFile);
+                createAudit(service.getServiceId(), module.getModuleName(), user.getUsername(), "Created file " + data.name + " in " + data.network);
             }
         }
 
         response.setStatus(200);
         request.setHandled(true);
+    }
+
+    private void createAudit(String serviceId, String moduleName, String requestor, String action) {
+        Map<String, String> notes = new HashMap<>();
+        notes.put("action", action);
+        Audit audit = new Audit();
+        audit.serviceId = serviceId;
+        audit.moduleName = moduleName;
+        audit.timePerformed = getGmt();
+        audit.timeRequested = getGmt();
+        audit.requestor = requestor;
+        audit.type = Type.module;
+        audit.operation = Operation.update;
+        audit.notes = getGson().toJson(notes);
+        getDataAccess().saveAudit(audit, null);
     }
 
 }
