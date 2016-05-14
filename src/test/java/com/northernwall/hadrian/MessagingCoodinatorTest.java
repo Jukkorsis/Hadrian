d7e4c5b84c760f9ccfb54e7d0f23e2a38c635354
Richard Thurston <richard@northernwall.com>
2016-05-14 13:06:38 -0700

    Fix test

--- src/test/java/com/northernwall/hadrian/MessagingCoodinatorTest.java
+++ src/test/java/com/northernwall/hadrian/MessagingCoodinatorTest.java
@@ -18,24 +18,35 @@ package com.northernwall.hadrian;
 import com.northernwall.hadrian.messaging.MessagingCoodinator;
 import com.northernwall.hadrian.stubs.StubMessageProcessor;
 import com.northernwall.hadrian.stubs.StubParameters;
+import com.squareup.okhttp.ConnectionPool;
+import com.squareup.okhttp.OkHttpClient;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import junit.framework.Assert;
 import org.junit.Test;
-
 
 /**
  *
  * @author rthursto
  */
 public class MessagingCoodinatorTest {
-    
+
+    private final OkHttpClient client;
+
     public MessagingCoodinatorTest() {
+        client = new OkHttpClient();
+        client.setConnectTimeout(2, TimeUnit.SECONDS);
+        client.setReadTimeout(2, TimeUnit.SECONDS);
+        client.setWriteTimeout(2, TimeUnit.SECONDS);
+        client.setFollowSslRedirects(false);
+        client.setFollowRedirects(false);
+        client.setConnectionPool(new ConnectionPool(5, 60 * 1000));
     }
-    
+
     @Test
     public void sendMessageTest() {
-        MessagingCoodinator mc = new MessagingCoodinator(new StubParameters());
+        MessagingCoodinator mc = new MessagingCoodinator(new StubParameters(), client);
         Map<String, String> data = new HashMap<>();
         data.put("A", "a");
         data.put("B", "b");
@@ -43,5 +54,5 @@ public class MessagingCoodinatorTest {
         mc.sendMessage("TEST", null, data);
         Assert.assertEquals("Hi a.", StubMessageProcessor.text);
     }
-    
+
 }
--- src/test/java/com/northernwall/hadrian/stubs/StubMessageProcessor.java
+++ src/test/java/com/northernwall/hadrian/stubs/StubMessageProcessor.java
@@ -1,21 +1,24 @@
 package com.northernwall.hadrian.stubs;
 
+import com.google.gson.Gson;
 import com.northernwall.hadrian.domain.Team;
 import com.northernwall.hadrian.messaging.MessageProcessor;
 import com.northernwall.hadrian.messaging.MessageType;
 import com.northernwall.hadrian.parameters.Parameters;
+import com.squareup.okhttp.OkHttpClient;
 import java.util.Map;
 
-public class StubMessageProcessor  extends MessageProcessor {
+public class StubMessageProcessor extends MessageProcessor {
+
     public static String text;
 
-        @Override
-        public void init(Parameters parameters) {
-        }
+    @Override
+    public void init(Parameters parameters, Gson gson, OkHttpClient client) {
+    }
 
-        @Override
-        public void process(MessageType messageType, Team team, Map<String, String> data) {
-            text = replaceTerms(messageType.emailBody, data);
-        }
+    @Override
+    public void process(MessageType messageType, Team team, Map<String, String> data) {
+        text = replaceTerms(messageType.emailBody, data);
+    }
 
 }
