4cd1c8a04b44014e3f54dff5cd5f6b0ccc7d5e54
Richard Thurston <richard@northernwall.com>
2016-06-28 21:37:37 -0700

    Added code to convert simulators

--- src/main/java/com/northernwall/hadrian/db/DataAccessUpdater.java
+++ src/main/java/com/northernwall/hadrian/db/DataAccessUpdater.java
@@ -1,5 +1,9 @@
 package com.northernwall.hadrian.db;
 
+import com.northernwall.hadrian.domain.Module;
+import com.northernwall.hadrian.domain.ModuleType;
+import com.northernwall.hadrian.domain.Service;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +17,23 @@ public class DataAccessUpdater {
         if (version == null) {
             dataAccess.setVersion("1.5");
             update(dataAccess);
+        }
+        
+        List<Service> services = dataAccess.getActiveServices();
+        if (services != null && !services.isEmpty()) {
+            for (Service service : services) {
+                List<Module> modules = dataAccess.getModules(service.getServiceId());
+                if (modules != null && !modules.isEmpty()) {
+                    for (Module module : modules) {
+                        if (module.getModuleName().toLowerCase().contains("simulator") 
+                                && module.getModuleType().equals(ModuleType.Deployable)) {
+                            logger.info("Found a sim, {} in {}", module.getModuleName(), service.getServiceName());
+                            module.setModuleType(ModuleType.Simulator);
+                            dataAccess.saveModule(module);
+                        }
+                    }
+                }
+            }
         }
         
         logger.info("Current DB version is {}, no upgrade required.", version);
