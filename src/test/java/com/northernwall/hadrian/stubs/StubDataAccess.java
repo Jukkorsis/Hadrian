446d1a3cba6f47b1c560cf1246404ee5e750b3c9
Jukkorsis <richard@northernwall.com>
2016-11-10 20:48:07 -0800

    Simplify the create Service forms

--- src/test/java/com/northernwall/hadrian/stubs/StubDataAccess.java
+++ src/test/java/com/northernwall/hadrian/stubs/StubDataAccess.java
@@ -90,6 +90,11 @@ public class StubDataAccess implements DataAccess {
     }
 
     @Override
+    public void updateService(Service service) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
     public void deleteServiceSearch(Service service) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
